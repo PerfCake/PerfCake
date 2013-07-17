14931c854f4897afffe4a7aebe3cfb9782857f9e
Pavel Macík <pavel.macik@gmail.com>
2013-07-17 11:29:44 +0200

    Added javadoc to ImmediateMessageGenerator class

--- src/test/java/org/perfcake/reporting/util/ObjectFactoryTest.java
+++ src/test/java/org/perfcake/reporting/util/ObjectFactoryTest.java
@@ -15,12 +15,10 @@ public class ObjectFactoryTest {
    public void testObjectSummoning() throws Throwable {
       Properties p = new Properties();
       p.setProperty("threads", "42");
-      p.setProperty("timeWindowSize", "123");
       p.setProperty("count", "10001");
       ImmediateMessageGenerator g = (ImmediateMessageGenerator) ObjectFactory.summonInstance("org.perfcake.message.generator.ImmediateMessageGenerator", p);
 
       Assert.assertEquals(42, g.getThreads());
-      Assert.assertEquals(123, g.getTimeWindowSize());
       Assert.assertEquals(10001L, g.getCount());
 
    }
