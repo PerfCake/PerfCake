42148a53199b4871b2ca780de95cf035f38929ed
Pavel Macík <pavel.macik@gmail.com>
2013-07-24 17:35:41 +0200

    HMSNumberFormat and the MeasurementTest updated and added a javadoc

--- src/main/java/org/perfcake/nreporting/util/HMSNumberFormat.java
+++ src/main/java/org/perfcake/nreporting/util/HMSNumberFormat.java
@@ -4,7 +4,16 @@ import java.text.DecimalFormat;
 import java.text.FieldPosition;
 import java.text.NumberFormat;
 import java.text.ParsePosition;
+import java.text.ParseException;
 
+/**
+ * The number format that is able to format a timestamp (in milliseconds)
+ * into a string in a form of "H:M:S", where H is unbounded number of hours, M is number of minutes
+ * in the hour and S is number of seconds in the minute.
+ * 
+ * @author Pavel Macík <pavel.macik@gmail.com>
+ * 
+ */
 public class HMSNumberFormat extends NumberFormat {
 
    /**
@@ -13,47 +22,53 @@ public class HMSNumberFormat extends NumberFormat {
    private static final long serialVersionUID = -1L;
 
    /**
-    * 
+    * Number of milliseconds in an hour.
     */
    public static final long MILLIS_IN_HOUR = 3600000L;
 
    /**
-    * 
+    * Number of milliseconds in a minute.
     */
    public static final long MILLIS_IN_MINUTE = 60000L;
 
    /**
-    * 
+    * Number of milliseconds in a second.
     */
    public static final long MILLIS_IN_SECOND = 1000L;
 
    /**
-    * 
+    * Pattern character representing hours.
     */
    private static final String HOURS = "H";
+
    /**
-    * 
+    * Pattern character representing minutes.
     */
    private static final String MINUTES = "M";
+
    /**
-    * 
+    * Pattern character representing seconds.
     */
    private static final String SECONDS = "S";
 
    /**
-    * 
+    * Number format for minutes and seconds to be rendered in 2 digits with padding 0.
     */
    private static NumberFormat numberFormat = new DecimalFormat("00");
 
    /**
-    * 
+    * The format pattern.
     */
    private String pattern;
 
    /**
-    * Creates a new instance of HMSNumberFormat.
+    * Creates a new instance of HMSNumberFormat using the specified {@link #pattern}.
+    * The pattern is converted into the resulting format where 'H' is replaced by the number of hours,
+    * 'M' is replaced by the number of minutes in the hour and 'S' is replaces by the number of seconds
+    * in the minute.
     * 
     * @param pattern
+    *           The formating pattern.
     */
    public HMSNumberFormat(String pattern) {
       super();
@@ -61,11 +76,11 @@ public class HMSNumberFormat extends NumberFormat {
    }
 
    /**
-    * Creates a new instance of HMSNumberFormat.
+    * Creates a new instance of HMSNumberFormat with the default pattern "H:M:S".
     */
    public HMSNumberFormat() {
       super();
-      this.pattern = null;
+      this.pattern = "H:M:S";
    }
 
    /*
@@ -89,15 +104,7 @@ public class HMSNumberFormat extends NumberFormat {
       number = number % MILLIS_IN_HOUR;
       long minutes = number / MILLIS_IN_MINUTE;
       long seconds = (number % MILLIS_IN_MINUTE) / 1000;
-      if (pattern == null) {
-         toAppendTo.append(hours);
-         toAppendTo.append(":");
-         toAppendTo.append(numberFormat.format(minutes));
-         toAppendTo.append(":");
-         toAppendTo.append(numberFormat.format(seconds));
-      } else {
-         toAppendTo.append(pattern.replaceAll(HOURS, String.valueOf(hours)).replaceAll(MINUTES, String.valueOf(numberFormat.format(minutes))).replaceAll(SECONDS, String.valueOf(numberFormat.format(seconds))));
-      }
+      toAppendTo.append(pattern.replaceAll(HOURS, String.valueOf(hours)).replaceAll(MINUTES, String.valueOf(numberFormat.format(minutes))).replaceAll(SECONDS, String.valueOf(numberFormat.format(seconds))));
       return toAppendTo;
    }
 
@@ -106,18 +113,34 @@ public class HMSNumberFormat extends NumberFormat {
     * 
     * @see java.text.NumberFormat#parse(java.lang.String, java.text.ParsePosition)
     */
+   /*
+    * (non-Javadoc)
+    * 
+    * @see java.text.NumberFormat#parse(java.lang.String, java.text.ParsePosition)
+    */
    @Override
    public Number parse(String source, ParsePosition parsePosition) {
-      return HMSNumberFormat.parseTimeStamp(source);
+      try {
+         return HMSNumberFormat.parseTimeStamp(source);
+      } catch (ParseException pe) {
+         pe.printStackTrace();
+      }
+      return null;
    }
 
    /**
+    * Parses the timestamp (in milliseconds) from a string in a form of "H:M:S".
+    * 
     * @param source
-    * @return
+    *           The string representing the time.
+    * @return The timestamp in milliseconds.
     */
-   public static long parseTimeStamp(String source) {
+   public static long parseTimeStamp(String source) throws ParseException {
       String[] timeTokens = source.split(":");
-      return Long.valueOf(timeTokens[0]) * MILLIS_IN_HOUR + Long.valueOf(timeTokens[1]) * MILLIS_IN_MINUTE + Long.valueOf(timeTokens[2]) * 1000;
+      if (timeTokens.length == 3) {
+         return Long.valueOf(timeTokens[0]) * MILLIS_IN_HOUR + Long.valueOf(timeTokens[1]) * MILLIS_IN_MINUTE + Long.valueOf(timeTokens[2]) * 1000;
+      } else {
+         throw new ParseException("Couldn't parse \"" + source + "\". It should be in a form of \"H:M:S\".", -1);
+      }
    }
-
 }
--- src/test/java/org/perfcake/nreporting/MeasurementTest.java
+++ src/test/java/org/perfcake/nreporting/MeasurementTest.java
@@ -1,6 +1,7 @@
 package org.perfcake.nreporting;
 
 import org.perfcake.nreporting.util.HMSNumberFormat;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MeasurementTest {
@@ -8,14 +9,17 @@ public class MeasurementTest {
    private static final long MINUTES = 42;
    private static final long SECONDS = 9;
 
+   private static final long PERCENTAGE = 15;
    private static final long TIMESTAMP = HOURS * HMSNumberFormat.MILLIS_IN_HOUR + MINUTES * HMSNumberFormat.MILLIS_IN_MINUTE + SECONDS * HMSNumberFormat.MILLIS_IN_SECOND;
+   private static final long ITERATIONS = 12345;
 
    @Test
    public void testMeasurement() {
-      Measurement m = new Measurement(15, TIMESTAMP, 12345);
+      Measurement m = new Measurement(PERCENTAGE, TIMESTAMP, ITERATIONS);
       m.set("18523.269 it/s");
       m.set("current", "257.58 it/s");
       m.set("average", "300.25 it/s");
-      System.out.println(m.toString());
+
+      Assert.assertEquals(m.toString(), "[" + HOURS + ":" + MINUTES + ":0" + SECONDS + "][" + ITERATIONS + " iterations][" + PERCENTAGE + "%] [18523.269 it/s] [current => 257.58 it/s] [average => 300.25 it/s]");
    }
 }
