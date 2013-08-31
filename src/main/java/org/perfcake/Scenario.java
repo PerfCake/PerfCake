cbbf33b8497be910fd27adbb27c519e4c5f8dae4
Martin Večeřa <marvenec@gmail.com>
2013-08-31 23:07:53 +0200

    prepared for 1.0-SNAPSHOT

--- src/main/java/org/perfcake/Scenario.java
+++ src/main/java/org/perfcake/Scenario.java
@@ -45,7 +45,7 @@ public class Scenario {
    private List<MessageTemplate> messageStore;
    private ValidatorManager validatorManager;
 
-   public static final String VERSION = "0.3";
+   public static final String VERSION = "1.0";
 
    /**
     * Initialize the scenario execution
