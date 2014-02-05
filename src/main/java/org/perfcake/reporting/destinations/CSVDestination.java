fb4b430d0512d527781367e2c769d36b649ff3b2
Martin Večeřa <marvenec@gmail.com>
2014-02-05 22:12:33 +0100

    optimized csvdestination performance

--- src/main/java/org/perfcake/reporting/destinations/CSVDestination.java
+++ src/main/java/org/perfcake/reporting/destinations/CSVDestination.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,9 +36,8 @@ import org.perfcake.util.Utils;
 
 /**
  * The destination that appends the {@link Measurement} into a CSV file.
- * 
+ *
  * @author Pavel Macík <pavel.macik@gmail.com>
- * 
  */
 public class CSVDestination implements Destination {
 
@@ -67,6 +66,11 @@ public class CSVDestination implements Destination {
     */
    private final List<String> resultNames = new ArrayList<>();
 
+   /**
+    * Cached headers in the CSV file
+    */
+   private String fileHeaders = null;
+
    @Override
    public void open() {
       csvFile = new File(path);
@@ -80,64 +84,88 @@ public class CSVDestination implements Destination {
       // nothing to do
    }
 
+   private void presetResultNames(final Measurement m) {
+      final StringBuilder sb = new StringBuilder();
+      final Map<String, Object> results = m.getAll();
+      final Object defaultResult = m.get();
+
+      for (String key : results.keySet()) {
+         if (!key.equals(Measurement.DEFAULT_RESULT)) {
+            resultNames.add(key);
+         }
+      }
+
+      sb.append("Time");
+      sb.append(delimiter);
+      sb.append("Iterations");
+      if (defaultResult != null) {
+         sb.append(delimiter);
+         sb.append(Measurement.DEFAULT_RESULT);
+      }
+      for (String key : resultNames) {
+         sb.append(delimiter);
+         sb.append(key);
+      }
+      sb.append("\n");
+
+      fileHeaders = sb.toString();
+   }
+
+   private String getResultsLine(final Measurement m) {
+      final Object defaultResult = m.get();
+      final Map<String, Object> results = m.getAll();
+      final StringBuilder sb = new StringBuilder();
+
+      sb.append(Utils.timeToHMS(m.getTime()));
+      sb.append(delimiter);
+      sb.append(m.getIteration() + 1);
+
+      if (defaultResult != null) {
+         sb.append(delimiter);
+         if (defaultResult instanceof Quantity<?>) {
+            sb.append(((Quantity<?>) defaultResult).getNumber());
+         } else {
+            sb.append(defaultResult);
+         }
+      }
+
+      Object currentResult;
+      for (String resultName : resultNames) {
+         sb.append(delimiter);
+         currentResult = results.get(resultName);
+         if (currentResult instanceof Quantity<?>) {
+            sb.append(((Quantity<?>) currentResult).getNumber());
+         } else {
+            sb.append(currentResult);
+         }
+      }
+
+      return sb.toString();
+   }
+
    @Override
    public void report(final Measurement m) throws ReportingException {
-      final String delim = delimiter; // not to have delimiter locked most of the time
-      synchronized (this) {
-         final StringBuffer sb = new StringBuffer();
-         final Map<String, Object> results = m.getAll();
-         final Object defaultResult = m.get();
-
-         if (resultNames.isEmpty()) {
-            for (String key : results.keySet()) {
-               if (!key.equals(Measurement.DEFAULT_RESULT)) {
-                  resultNames.add(key);
-               }
+      if (resultNames.isEmpty()) { // performance optimization before we enter the sync. block
+         synchronized (resultNames) {
+            if (resultNames.isEmpty()) { // make sure the array did not get initialized while we were entering the sync. block
+               presetResultNames(m);
             }
          }
+      }
 
+      final String resultLine = getResultsLine(m);
+
+      synchronized (this) {
          // make sure the order of columns is consistent
          final boolean csvFileExists = csvFile.exists();
-         if (!csvFileExists) {
-            sb.append("Time");
-            sb.append(delim);
-            sb.append("Iterations");
-            if (defaultResult != null) {
-               sb.append(delim);
-               sb.append(Measurement.DEFAULT_RESULT);
+         try (FileOutputStream fos = new FileOutputStream(csvFile, true);
+               OutputStreamWriter osw = new OutputStreamWriter(fos, Utils.getDefaultEncoding());
+               BufferedWriter bw = new BufferedWriter(osw)) {
+            if (!csvFileExists) {
+               bw.append(fileHeaders);
+               bw.newLine();
             }
-            for (String key : resultNames) {
-               sb.append(delim);
-               sb.append(key);
-            }
-            sb.append("\n");
-         }
-
-         sb.append(Utils.timeToHMS(m.getTime()));
-         sb.append(delim);
-         sb.append(m.getIteration() + 1);
-         if (defaultResult != null) {
-            sb.append(delim);
-            if (defaultResult instanceof Quantity<?>) {
-               sb.append(((Quantity<?>) defaultResult).getNumber());
-            } else {
-               sb.append(defaultResult);
-            }
-         }
-
-         Object currentResult;
-         for (String resultName : resultNames) {
-            sb.append(delim);
-            currentResult = results.get(resultName);
-            if (currentResult instanceof Quantity<?>) {
-               sb.append(((Quantity<?>) currentResult).getNumber());
-            } else {
-               sb.append(currentResult);
-            }
-         }
-
-         try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(csvFile, true), Utils.getDefaultEncoding()))) {
-            bw.append(sb.toString());
+            bw.append(resultLine);
             bw.newLine();
          } catch (IOException ioe) {
             throw new ReportingException("Could not append a report to the file: " + csvFile.getPath(), ioe);
@@ -148,7 +176,7 @@ public class CSVDestination implements Destination {
 
    /**
     * Used to read the value of path.
-    * 
+    *
     * @return The path value.
     */
    public String getPath() {
@@ -157,9 +185,9 @@ public class CSVDestination implements Destination {
 
    /**
     * Used to set the value of path.
-    * 
+    *
     * @param path
-    *           The path value to set.
+    *       The path value to set.
     */
    public void setPath(final String path) {
       this.path = path;
@@ -168,7 +196,7 @@ public class CSVDestination implements Destination {
 
    /**
     * Used to read the value of delimiter.
-    * 
+    *
     * @return The delimiter value.
     */
    public String getDelimiter() {
@@ -177,9 +205,9 @@ public class CSVDestination implements Destination {
 
    /**
     * Used to set the value of delimiter.
-    * 
+    *
     * @param delimiter
-    *           The delimiter value to set.
+    *       The delimiter value to set.
     */
    public void setDelimiter(final String delimiter) {
       this.delimiter = delimiter;
