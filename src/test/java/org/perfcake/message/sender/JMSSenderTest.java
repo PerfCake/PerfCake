459f64c9616bcec31ad0bf773404d3e67a61278e
Lenka Vaskova <lvaskova@redhat.com>
2014-05-04 13:32:50 +0200

    #112 initial Arquillian setup

---
+++ src/test/java/org/perfcake/message/sender/JMSSenderTest.java
@@ -0,0 +1,63 @@
+/*
+ * -----------------------------------------------------------------------\
+ * PerfCake
+ *  
+ * Copyright (C) 2010 - 2013 the original author or authors.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------/
+ */
+package org.perfcake.message.sender;
+
+import javax.annotation.Resource;
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ * @author Lenka Vašková <vaskova.lenka@gmail.com>
+ * 
+ */
+public class JMSSenderTest extends Arquillian {
+
+   @Resource(mappedName = "jms/queue/testQueue")
+   private Queue queue;
+
+   @Resource
+   private ConnectionFactory factory;
+
+   @Deployment(name = "JMSSenderTest.war")
+   public static JavaArchive createDeployment() {
+      return ShrinkWrap.create(JavaArchive.class).addAsResource("beans.xml");
+   }
+
+   @Deployment(name = "queue.jar")
+   public static JavaArchive createQueueDeployment() {
+      return ShrinkWrap.create(JavaArchive.class).addAsResource("testQueue-jms.xml", "META-INF/testQueue-jms.xml");
+   }
+
+   @Test
+   public void testJMS() throws Exception {
+
+      System.out.println("************************ Connection factory value: " + factory);
+      System.out.println("************************ Queue value: " + queue);
+
+   }
+
+}
