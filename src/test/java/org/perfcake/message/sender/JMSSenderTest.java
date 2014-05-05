f94f1e765144ead6ba2c3ccfc65a6d97633f83b4
Lenka Vaskova <lvaskova@redhat.com>
2014-05-05 20:28:15 +0200

    switched to AS7

--- src/test/java/org/perfcake/message/sender/JMSSenderTest.java
+++ src/test/java/org/perfcake/message/sender/JMSSenderTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,26 +30,19 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.testng.annotations.Test;
 
 /**
- * 
  * @author Lenka Vašková <vaskova.lenka@gmail.com>
- * 
  */
 public class JMSSenderTest extends Arquillian {
 
-   @Resource(mappedName = "jms/queue/testQueue")
+   @Resource(mappedName = "queue/test")
    private Queue queue;
 
-   @Resource
+   @Resource(mappedName = "java:/ConnectionFactory")
    private ConnectionFactory factory;
 
-   @Deployment(name = "JMSSenderTest.war")
+   @Deployment
    public static JavaArchive createDeployment() {
-      return ShrinkWrap.create(JavaArchive.class).addAsResource("beans.xml");
-   }
-
-   @Deployment(name = "queue.jar")
-   public static JavaArchive createQueueDeployment() {
-      return ShrinkWrap.create(JavaArchive.class).addAsResource("testQueue-jms.xml", "META-INF/testQueue-jms.xml");
+      return ShrinkWrap.create(JavaArchive.class); // Arquillian needs this to deploy the test
    }
 
    @Test
