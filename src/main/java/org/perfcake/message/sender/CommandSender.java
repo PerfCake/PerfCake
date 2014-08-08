5828d9c1ccd83003773f440bf0429379e9ab4fc2
Pavel Macík <pavel.macik@gmail.com>
2014-08-08 15:00:51 +0200

    #127: CommandSender - fixed cases when message or it's payload is null.

--- src/main/java/org/perfcake/message/sender/CommandSender.java
+++ src/main/java/org/perfcake/message/sender/CommandSender.java
@@ -35,7 +35,7 @@ import org.perfcake.util.Utils;
 
 /**
  * The sender that can invoke external command (specified by {@link #target} property)
- * in a separate process to send the message payload passed to the standard input of
+ * in a separate process to send the message payload (if message is specified) passed to the standard input of
  * the process or as the command argument.
  *
  * @author Martin Večeřa <marvenec@gmail.com>
@@ -100,27 +100,47 @@ public class CommandSender extends AbstractSender {
    @Override
    public void preSend(final Message message, final Map<String, String> properties) throws Exception {
       super.preSend(message, properties);
-      this.messagePayload = message.getPayload().toString();
-      command = (commandPrefix + " " + target + (messageFrom == MessageFrom.ARGUMENTS ? " " + message.getPayload() : "")).trim();
+      if (message != null) {
+         final Serializable payload = message.getPayload();
+         if (payload != null) {
+            messagePayload = payload.toString();
+         } else {
+            messagePayload = null;
+         }
+      } else {
+         messagePayload = null;
+      }
 
-      Set<Entry<String, String>> propertiesEntrySet = properties.entrySet();
-      environmentVariables = new String[propertiesEntrySet.size() + message.getHeaders().size() + message.getProperties().size()];
+      if (messagePayload != null && messageFrom == MessageFrom.ARGUMENTS) {
+         command = (commandPrefix + " " + target + " " + messagePayload).trim();
+      } else {
+         command = (commandPrefix + " " + target).trim();
+      }
+
+      final Set<Entry<String, String>> propertiesEntrySet = properties.entrySet();
+      final Set<Entry<String, String>> envEntrySet = System.getenv().entrySet();
+      environmentVariables = new String[propertiesEntrySet.size() + envEntrySet.size() + (message != null ? message.getHeaders().size() + message.getProperties().size() : 0)];
       int i = 0;
       for (Entry<String, String> entry : propertiesEntrySet) {
          environmentVariables[i++] = entry.getKey() + "=" + entry.getValue();
       }
-      for (Entry<Object, Object> entry : message.getHeaders().entrySet()) {
+      for (Entry<String, String> entry : envEntrySet) {
          environmentVariables[i++] = entry.getKey() + "=" + entry.getValue();
       }
-      for (Entry<Object, Object> entry : message.getProperties().entrySet()) {
-         environmentVariables[i++] = entry.getKey() + "=" + entry.getValue();
+      if (message != null) {
+         for (Entry<Object, Object> entry : message.getHeaders().entrySet()) {
+            environmentVariables[i++] = entry.getKey() + "=" + entry.getValue();
+         }
+         for (Entry<Object, Object> entry : message.getProperties().entrySet()) {
+            environmentVariables[i++] = entry.getKey() + "=" + entry.getValue();
+         }
       }
    }
 
    @Override
    public Serializable doSend(final Message message, final Map<String, String> properties, final MeasurementUnit mu) throws Exception {
       process = Runtime.getRuntime().exec(command, environmentVariables);
-      if (messageFrom == MessageFrom.STDIN) {
+      if (messagePayload != null && messageFrom == MessageFrom.STDIN) {
          writer = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(process.getOutputStream()), Utils.getDefaultEncoding()), true);
          writer.write(messagePayload);
          writer.flush();
@@ -201,5 +221,4 @@ public class CommandSender extends AbstractSender {
    public void setEnvironmentVariables(String[] environmentVariables) {
       this.environmentVariables = Arrays.copyOf(environmentVariables, environmentVariables.length); // ignore any later external modifications
    }
-
 }
