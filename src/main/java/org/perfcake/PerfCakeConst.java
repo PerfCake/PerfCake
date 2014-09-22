536152a381461d5c4ea0258e94468ffcea5b0160
Martin Večeřa <marvenec@gmail.com>
2014-09-22 23:16:54 +0200

    correct version displaying

--- src/main/java/org/perfcake/PerfCakeConst.java
+++ src/main/java/org/perfcake/PerfCakeConst.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,9 @@ package org.perfcake;
  * @author Pavel Macík <pavel.macik@gmail.com>
  */
 public final class PerfCakeConst {
+   public static final String VERSION = "3.2";
+   public static final String XSD_SCHEMA_VERSION = "3.0";
+
    public static final String MESSAGE_NUMBER_HEADER = "PerfCake_Performance_Message_Number";
    public static final String MESSAGE_NUMBER_PROPERTY = "MessageNumber";
 
