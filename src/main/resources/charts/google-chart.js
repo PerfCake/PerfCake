729764541545ed0bc47c0d273d1acd028a671746
Martin Večeřa <marvenec@gmail.com>
2015-01-12 22:01:45 +0100

    #116 first draft of a chart destination

---
+++ src/main/resources/charts/google-chart.js
@@ -0,0 +1,43 @@
+function drawChart(dataArray, chartDiv, columns, xAxis, yAxis, chartName) {
+   var data = google.visualization.arrayToDataTable(dataArray);
+   var options = {
+      enableInteractivity : true,
+      title : chartName,
+      height : 500,
+      hAxis : {
+         title : xAxis,
+         minValue : 0.0,
+         gridlines : {
+            count : 15
+         },
+         minorGridlines : {
+            count : 11
+         },
+         //format : '#.#####'
+      },
+      vAxis : {
+         title : yAxis,
+         minValue : 0.0,
+         gridlines : {
+            count : 13
+         },
+         minorGridlines : {
+            count : 1
+         },
+         format : "#.#"
+      },
+      linewidth : 1,
+      pointSize : 0,
+   }
+   var view = new google.visualization.DataView(data)
+   view.setColumns(columns)
+
+   var chart = new google.visualization.ChartWrapper({
+      'chartType': 'LineChart',
+      'dataTable': view,
+      'containerId': chartDiv,
+      'options': options,
+   });
+
+   chart.draw();
+}
---
+++ src/test/java/org/perfcake/reporting/destinations/ChartDestinationTest.java
@@ -0,0 +1,66 @@
+/*
+ * -----------------------------------------------------------------------\
+ * PerfCake
+ *  
+ * Copyright (C) 2010 - 2013 the original author or authors.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------/
+ */
+package org.perfcake.reporting.destinations;
+
+import org.perfcake.PerfCakeConst;
+import org.perfcake.reporting.Measurement;
+
+import org.testng.annotations.Test;
+
+/**
+ * Chart destination tests.
+ *
+ * @author Martin Večeřa <marvenec@gmail.com>
+ */
+public class ChartDestinationTest {
+
+   @Test(enabled = false)
+   public void basicTest() throws Exception {
+      System.setProperty(PerfCakeConst.TIMESTAMP_PROPERTY, String.valueOf(System.currentTimeMillis()));
+
+      ChartDestination cd = new ChartDestination();
+      cd.setTarget("test-chart");
+      cd.setXAxis("Time of test");
+      cd.setYAxis("Iterations per second");
+      cd.setName("Performance");
+      cd.setGroup("stats");
+      cd.setAttributes("Average, Result");
+
+      cd.open();
+
+      Measurement m = new Measurement(1, System.currentTimeMillis(), 1);
+      m.set(10.3);
+      m.set("Average", 9.8);
+      cd.report(m);
+
+      m = new Measurement(2, System.currentTimeMillis(), 2);
+      m.set(11.1);
+      m.set("Average", 9.1);
+      cd.report(m);
+
+      m = new Measurement(3, System.currentTimeMillis(), 3);
+      m.set(9.2);
+      m.set("Average", 9.0);
+      cd.report(m);
+
+      cd.close();
+   }
+
+}
