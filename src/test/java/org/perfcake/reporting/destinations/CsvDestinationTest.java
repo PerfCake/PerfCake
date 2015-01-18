937b37727fa0eaa694c50e7d09c9c91f21075e7d
Martin Večeřa <marvenec@gmail.com>
2015-01-18 21:38:25 +0100

    fixed CsvDestinationTest formatting

--- src/test/java/org/perfcake/reporting/destinations/CsvDestinationTest.java
+++ src/test/java/org/perfcake/reporting/destinations/CsvDestinationTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,13 +19,14 @@
  */
 package org.perfcake.reporting.destinations;
 
-import org.apache.log4j.Logger;
 import org.perfcake.PerfCakeConst;
 import org.perfcake.TestSetup;
 import org.perfcake.reporting.Measurement;
 import org.perfcake.reporting.Quantity;
 import org.perfcake.reporting.ReportingException;
 import org.perfcake.util.ObjectFactory;
+
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -49,10 +50,10 @@ import java.util.Scanner;
 public class CsvDestinationTest {
 
    private static final long ITERATION = 12345;
-   private File csvFile;
    private static final String TIMESTAMP = String.valueOf(Calendar.getInstance().getTimeInMillis());
    private static final String DEFAULT_PATH = "perfcake-results-" + TIMESTAMP + ".csv";
    private static final Logger log = Logger.getLogger(CsvDestinationTest.class);
+   private File csvFile;
 
    @BeforeClass
    public void beforeClass() throws IOException {
