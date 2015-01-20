021143d26b2d41fff9de7f606e83d06c67b2a8fa
Martin Večeřa <marvenec@gmail.com>
2015-01-20 15:25:36 +0100

    #68 fixed imports

--- src/main/java/org/perfcake/ScenarioExecution.java
+++ src/main/java/org/perfcake/ScenarioExecution.java
@@ -30,6 +30,7 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
