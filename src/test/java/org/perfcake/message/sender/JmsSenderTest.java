4ef81b2d1b3aa3b23eee7b99ba455c45a6de52ea
Martin Večeřa <marvenec@gmail.com>
2015-01-21 18:41:53 +0100

    fixed typost in test

--- src/test/java/org/perfcake/message/sender/JmsSenderTest.java
+++ src/test/java/org/perfcake/message/sender/JmsSenderTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -77,7 +77,7 @@ public class JmsSenderTest extends Arquillian {
    @Test
    public void testBasicSend() throws Exception {
       Properties props = new Properties();
-      props.setProperty("messagetType", "STRING");
+      props.setProperty("messageType", "STRING");
       props.setProperty("target", "queue/test");
 
       JmsSender sender = (JmsSender) ObjectFactory.summonInstance(JmsSender.class.getName(), props);
@@ -223,7 +223,7 @@ public class JmsSenderTest extends Arquillian {
    @Test
    public void testClientAck() throws Exception {
       Properties props = new Properties();
-      props.setProperty("messagetType", "STRING");
+      props.setProperty("messageType", "STRING");
       props.setProperty("target", "queue/test");
 
       JmsSender sender = (JmsSender) ObjectFactory.summonInstance(JmsSender.class.getName(), props);
