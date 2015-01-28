296f8615d7931028db5bdb02465a96e81cc9d7da
Martin Večeřa <marvenec@gmail.com>
2015-01-28 22:04:06 +0100

    #116 the charts are now merged properly

---
+++ src/main/resources/charts/google-chart-concat.js
@@ -0,0 +1,49 @@
+function drawConcatChart(dataArray, chartDiv, columns, xAxis, yAxis, chartName) {
+   var data = new google.visualization.DataTable();
+   data.addColumn('string', dataArray[0][0]);
+   for (var i = 1; i < dataArray[0].length; i++) {
+      data.addColumn('number', dataArray[0][i]);
+   }
+
+   data.addRows(dataArray.slice(1));
+   var options = {
+      enableInteractivity : true,
+      title : chartName,
+      height : 500,
+      hAxis : {
+         title : xAxis,
+         minValue : 0.0,
+         gridlines : {
+            count : 15
+         },
+         minorGridlines : {
+            count : 11
+         },
+         //format : '#.#####'
+      },
+      vAxis : {
+         title : yAxis,
+         minValue : 0.0,
+         gridlines : {
+            count : 13
+         },
+         minorGridlines : {
+            count : 1
+         },
+         format : "#.#"
+      },
+      linewidth : 1,
+      pointSize : 0,
+   }
+   var view = new google.visualization.DataView(data)
+   view.setColumns(columns)
+
+   var chart = new google.visualization.ChartWrapper({
+      'chartType': 'LineChart',
+      'dataTable': view,
+      'containerId': chartDiv,
+      'options': options,
+   });
+
+   chart.draw();
+}
