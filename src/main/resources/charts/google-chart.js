ef05b5fc9bf847fc86aad6e2ee46f39ecbd2f0cb
Martin Večeřa <marvenec@gmail.com>
2015-02-08 20:57:04 +0100

    #116 preliminary version of correct tooltips

--- src/main/resources/charts/google-chart.js
+++ src/main/resources/charts/google-chart.js
@@ -1,4 +1,6 @@
 function drawChart(dataArray, chartDiv, columns, xAxisType, xAxis, yAxis, chartName) {
+   var offset = (new Date()).getTimezoneOffset() * 60 * 1000;
+
    var data = new google.visualization.DataTable();
    for (var i = 0; i < dataArray[0].length; i++) {
       if (i == 0 && xAxisType == 1) {
@@ -9,10 +11,31 @@ function drawChart(dataArray, chartDiv, columns, xAxisType, xAxis, yAxis, chartN
          data.addColumn('number', dataArray[0][i]);
       }
    }
+   data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+
+   for (var i = 1; i < dataArray.length; i++) {
+      var legend;
+
+      if (xAxisType == 1) {
+         legend = '<strong>Time: ' + (dataArray[i][0].getTime() - offset);
+      } else {
+         legend = '<strong>' + dataArray[0][0] + ': ' + dataArray[i][0];
+          if (xAxisType == 2) {
+            legend = legend + '%';
+          }
+      }
+      legend = legend + '</strong><br />';
+
+      for (var j = 1; j < columns.length; j++) {
+         legend = legend + dataArray[0][columns[j]] + ': ' + dataArray[i][columns[j]] + '<br/ >';
+      }
+
+      dataArray[i].push(legend);
+   }
+
+   columns.push(dataArray[0].length);
 
    data.addRows(dataArray.slice(1));
-
-   var offset = (new Date()).getTimezoneOffset() * 60 * 1000;
 
    var options = {
       enableInteractivity : true,
@@ -44,6 +67,8 @@ function drawChart(dataArray, chartDiv, columns, xAxisType, xAxis, yAxis, chartN
       pointSize : 0,
       interpolateNulls: true,
       allowHtml: true,
+      tooltip: { isHtml: true },
+      focusTarget: 'category',
    }
    var view = new google.visualization.DataView(data);
    view.setColumns(columns);
