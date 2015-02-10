143e873ed9f4d9cadb9eea6af51e8589dad497f3
Martin Večeřa <marvenec@gmail.com>
2015-02-10 22:35:28 +0100

    fixed measurement test

--- src/test/java/org/perfcake/reporting/MeasurementTest.java
+++ src/test/java/org/perfcake/reporting/MeasurementTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,7 +40,7 @@ public class MeasurementTest {
       m.set("current", "257.58 it/s");
       m.set("average", "300.25 it/s");
 
-      Assert.assertEquals(m.toString(), "[" + HOURS + ":" + MINUTES + ":0" + SECONDS + "][" + ITERATIONS + " iterations][" + PERCENTAGE + "%] [18523.269 it/s] [current => 257.58 it/s] [average => 300.25 it/s]");
+      Assert.assertEquals(m.toString(), "[" + HOURS + ":" + MINUTES + ":0" + SECONDS + "][" + ITERATIONS + " iterations][" + PERCENTAGE + "%] [18523.269 it/s] [average => 300.25 it/s] [current => 257.58 it/s]");
    }
 
 }
