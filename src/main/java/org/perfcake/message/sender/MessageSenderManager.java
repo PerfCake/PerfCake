8c24ab9ea626139c907e98aa5818086b8a3d9827
Martin Večeřa <marvenec@gmail.com>
2015-03-12 22:15:51 +0100

    #150 message sender manager

--- src/main/java/org/perfcake/message/sender/MessageSenderManager.java
+++ src/main/java/org/perfcake/message/sender/MessageSenderManager.java
@@ -36,26 +36,73 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  */
 public class MessageSenderManager {
 
+   /**
+    * Number of available senders ready to send the message. Internally configured to reflect number of threads configured in a generator.
+    */
    private int senderPoolSize = 100;
+
+   /**
+    * Name of the class implementing the sender used in the scenario execution.
+    */
    private String senderClass;
+
+   /**
+    * Properties of message sender that will be passed to sender instances.
+    */
    private final Properties messageSenderProperties = new Properties();
+
+   /**
+    * Senders available to send messages.
+    */
    private final Queue<MessageSender> availableSenders = new ConcurrentLinkedQueue<>();
+
+   /**
+    * All the senders created including those actually busy.
+    */
    private final List<MessageSender> allSenders = new ArrayList<>();
 
+   /**
+    * Sets a message sender property.
+    *
+    * @param property
+    *       Property name to be set.
+    * @param value
+    *       A new property string value.
+    */
    public void setMessageSenderProperty(final String property, final String value) {
       messageSenderProperties.put(property, value);
    }
 
+   /**
+    * Sets a message sender property.
+    *
+    * @param property
+    *       Object referencing the property to be set.
+    * @param value
+    *       A new property value.
+    */
    public void setMessageSenderProperty(final Object property, final Object value) {
       messageSenderProperties.put(property, value);
    }
 
-   public void addMessageSenderProperties(final Properties props) {
-      if (props != null) {
-         messageSenderProperties.putAll(props);
+   /**
+    * Copies properties to message sender properties.
+    *
+    * @param properties
+    *       Properties to be added.
+    */
+   public void addMessageSenderProperties(final Properties properties) {
+      if (properties != null) {
+         messageSenderProperties.putAll(properties);
       }
    }
 
+   /**
+    * Initializes the message sender by creating all the message sender instances.
+    *
+    * @throws Exception
+    *       When it was not possible to create the instances.
+    */
    public void init() throws Exception {
       availableSenders.clear();
       for (int i = 0; i < senderPoolSize; i++) {
@@ -67,7 +114,8 @@ public class MessageSenderManager {
    /**
     * Adds {@link MessageSender} into available senders and initializes it.
     *
-    * @param sender Sender to be registered with this manager.
+    * @param sender
+    *       Sender to be registered with this manager.
     * @throws Exception
     *       When the initialization of the sender fails.
     */
@@ -77,7 +125,14 @@ public class MessageSenderManager {
       allSenders.add(sender);
    }
 
-   public MessageSender acquireSender() throws Exception {
+   /**
+    * Gets a free sender from the pool.
+    *
+    * @return A sender that is ready to send a message.
+    * @throws org.perfcake.PerfCakeException
+    *       When the pool is empty.
+    */
+   public MessageSender acquireSender() throws PerfCakeException {
       final MessageSender ms = availableSenders.poll();
       if (ms != null) {
          return ms;
@@ -86,10 +141,19 @@ public class MessageSenderManager {
       }
    }
 
+   /**
+    * Returns a sender that has been already used to the pool of available senders for later reuse.
+    *
+    * @param messageSender
+    *       The sender to be returned to the pool.
+    */
    public void releaseSender(final MessageSender messageSender) {
       availableSenders.offer(messageSender);
    }
 
+   /**
+    * Release all senders that has been acquired previously.
+    */
    public void releaseAllSenders() {
       for (final MessageSender ms : allSenders) {
          if (!availableSenders.contains(ms)) {
@@ -98,28 +162,61 @@ public class MessageSenderManager {
       }
    }
 
+   /**
+    * Gets the number of available senders in the pool.
+    *
+    * @return The number of available senders in the pool.
+    */
    public int availableSenderCount() {
       return availableSenders.size();
    }
 
+   /**
+    * Finalizes the message sender manager and disconnects all message senders from their target.
+    *
+    * @throws PerfCakeException
+    *       When the disconnection operation failed.
+    */
    public void close() throws PerfCakeException {
       for (final MessageSender ms : allSenders) {
          ms.close();
       }
    }
 
+   /**
+    * Gets the size of the pool of senders.
+    *
+    * @return The size of the pool of senders.
+    */
    public int getSenderPoolSize() {
       return senderPoolSize;
    }
 
+   /**
+    * Sets the size of the pool of senders.
+    *
+    * @param senderPoolSize
+    *       The size of the pool of senders.
+    */
    public void setSenderPoolSize(final int senderPoolSize) {
       this.senderPoolSize = senderPoolSize;
    }
 
+   /**
+    * Gets the name of the class implementing the message sender.
+    *
+    * @return The name of the class implementing the message sender.
+    */
    public String getSenderClass() {
       return senderClass;
    }
 
+   /**
+    * Sets the name of the class implementing the message sender.
+    *
+    * @param senderClass
+    *       The name of the class implementing the message sender.
+    */
    public void setSenderClass(final String senderClass) {
       this.senderClass = senderClass;
    }
