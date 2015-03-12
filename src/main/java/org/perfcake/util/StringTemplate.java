6a4c45da17b873d3452329938b9a97faa57c3f36
Pavel Macík <pavel.macik@gmail.com>
2015-03-12 15:24:15 +0100

    #150: Updated javadoc of StringTemplate.

--- src/main/java/org/perfcake/util/StringTemplate.java
+++ src/main/java/org/perfcake/util/StringTemplate.java
@@ -33,7 +33,7 @@ import httl.Engine;
 import httl.Template;
 
 /**
- * A string template that can quickly replace properties in form of ${property} and #{property} to their values.
+ * Holds a template capable of replacing properties in form of ${property} and #{property} to their values.
  * The properties with the dollar sign are replaced only once, while the properties with the hash sign are replaced with each call
  * to {@link #toString()} with the current values (this simulates JavaEE EL). System properties can be accessed using the props. prefix,
  * and environment properties can be accessed using the env. prefix.
