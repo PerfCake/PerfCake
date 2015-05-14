770e08d660dce7882a5df65c6cd80acd17802fd2
Martin Večeřa <marvenec@gmail.com>
2015-05-14 23:50:05 +0200

    bumped version of perfcake to 5.0

--- perfcake/src/main/java/org/perfcake/PerfCakeConst.java
+++ perfcake/src/main/java/org/perfcake/PerfCakeConst.java
@@ -29,12 +29,12 @@ public final class PerfCakeConst {
    /**
     * PerfCake version.
     */
-   public static final String VERSION = "4.1";
+   public static final String VERSION = "5.0";
 
    /**
     * PerfCake scenario XML Schema version that is part of namespace <code>urn:perfcake:scenario:&lt;version&gt;</code>.
     */
-   public static final String XSD_SCHEMA_VERSION = "4.0";
+   public static final String XSD_SCHEMA_VERSION = "5.0";
 
    /**
     * Name of the message header that stores message number.
