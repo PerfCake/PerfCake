4fe03af6322fed4564bf1908255009eca734eea9
Martin Večeřa <marvenec@gmail.com>
2015-05-17 01:09:52 +0200

    #197 completed number sequence test

--- perfcake/src/test/java/org/perfcake/message/sequences/NumberSequenceTest.java
+++ perfcake/src/test/java/org/perfcake/message/sequences/NumberSequenceTest.java
@@ -3,12 +3,14 @@ package org.perfcake.message.sequences;
 import org.perfcake.PerfCakeException;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  * @author Martin Večeřa <marvenec@gmail.com>
  */
 public class NumberSequenceTest {
 
+   @Test
    public void defaultTest() throws PerfCakeException {
       final NumberSequence s = new NumberSequence();
       s.reset();
@@ -23,6 +25,7 @@ public class NumberSequenceTest {
       Assert.assertEquals(s.getNext(), "2");
    }
 
+   @Test
    public void overUnderFlowTest() throws PerfCakeException {
       NumberSequence s = new NumberSequence();
       s.setStart(Long.MAX_VALUE - 8);
@@ -31,12 +34,47 @@ public class NumberSequenceTest {
 
       Assert.assertEquals(s.getNext(), String.valueOf(Long.MAX_VALUE - 8));
       Assert.assertEquals(s.getNext(), String.valueOf(Long.MAX_VALUE - 3));
-      Assert.assertEquals(s.getNext(), String.valueOf(Long.MIN_VALUE + 2));
-
+      Assert.assertEquals(s.getNext(), String.valueOf(Long.MIN_VALUE + 1));
 
       s.setStart(Long.MIN_VALUE + 8);
       s.setStep(-5);
       s.reset();
+
+      Assert.assertEquals(s.getNext(), String.valueOf(Long.MIN_VALUE + 8));
+      Assert.assertEquals(s.getNext(), String.valueOf(Long.MIN_VALUE + 3));
+      Assert.assertEquals(s.getNext(), String.valueOf(Long.MAX_VALUE - 1));
+   }
+
+   @Test
+   public void incTest() throws PerfCakeException {
+      NumberSequence s = new NumberSequence();
+      s.setStart(10);
+      s.setEnd(20);
+      s.setStep(3);
+      s.reset();
+
+      Assert.assertEquals(s.getNext(), "10");
+      Assert.assertEquals(s.getNext(), "13");
+      Assert.assertEquals(s.getNext(), "16");
+      Assert.assertEquals(s.getNext(), "19");
+      Assert.assertEquals(s.getNext(), "10");
+      Assert.assertEquals(s.getNext(), "13");
+   }
+
+   @Test
+   public void decTest() throws PerfCakeException {
+      NumberSequence s = new NumberSequence();
+      s.setStart(20);
+      s.setEnd(10);
+      s.setStep(-3);
+      s.reset();
+
+      Assert.assertEquals(s.getNext(), "20");
+      Assert.assertEquals(s.getNext(), "17");
+      Assert.assertEquals(s.getNext(), "14");
+      Assert.assertEquals(s.getNext(), "11");
+      Assert.assertEquals(s.getNext(), "20");
+      Assert.assertEquals(s.getNext(), "17");
    }
 
 }
