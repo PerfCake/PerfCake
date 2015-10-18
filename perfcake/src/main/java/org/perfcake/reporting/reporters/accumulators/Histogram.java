896e1b88f09d2d4af0ff42526e52ee2401f6e2f2
Martin Večeřa <marvenec@gmail.com>
2015-10-19 00:02:58 +0200

    fixed findbugs warnings

--- perfcake/src/main/java/org/perfcake/reporting/reporters/accumulators/Histogram.java
+++ perfcake/src/main/java/org/perfcake/reporting/reporters/accumulators/Histogram.java
@@ -120,6 +120,7 @@ public class Histogram {
     *
     * @return The actual counts for individual ranges of the histogram.
     */
+   @edu.umd.cs.findbugs.annotations.SuppressWarnings(value = "UC_USELESS_OBJECT", justification = "We copied the histogram hash map to get a frozen state.")
    public Map<Range, Long> getHistogram() {
       Map<Range, Long> result = new LinkedHashMap<>();
       Map<Range, AtomicLong> snapshot = new LinkedHashMap<>(histogram);
@@ -134,6 +135,7 @@ public class Histogram {
     *
     * @return The actual counts for individual ranges of the histogram.
     */
+   @edu.umd.cs.findbugs.annotations.SuppressWarnings(value = "UC_USELESS_OBJECT", justification = "We copied the histogram hash map to get a frozen state.")
    public Map<Range, Double> getHistogramInPercent() {
       Map<Range, Double> result = new LinkedHashMap<>();
       Map<Range, AtomicLong> snapshot = new LinkedHashMap<>(histogram);
