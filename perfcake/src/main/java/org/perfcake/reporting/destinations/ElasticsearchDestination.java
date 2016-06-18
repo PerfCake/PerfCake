5e89a82bc189834c59ed544ed6096ef217eb7838
Martin Večeřa <marvenec@gmail.com>
2016-06-18 21:14:36 +0200

    #303 completed the destination

--- perfcake/src/main/java/org/perfcake/reporting/destinations/ElasticsearchDestination.java
+++ perfcake/src/main/java/org/perfcake/reporting/destinations/ElasticsearchDestination.java
@@ -42,43 +42,90 @@ import java.util.stream.Collectors;
 
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
-import io.searchbox.client.JestResultHandler;
+import io.searchbox.client.JestResult;
 import io.searchbox.client.config.HttpClientConfig;
-import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
-import io.searchbox.indices.mapping.PutMapping;
+import io.searchbox.indices.CreateIndex;
 
 /**
+ * Writes the resulting data to Elasticsearch using a simple HTTP client.
+ * The reported data have information about the test progress (time in milliseconds since start, percentage and iteration),
+ * real time of each result, and the complete results map. Quantities are stored without their unit.
+ * To properly search through the data, we need to set the mapping (to be able to interpret time as time).
+ * However, this needs to be done just once for each index and type.
+ *
  * @author <a href="mailto:marvenec@gmail.com">Martin Večeřa</a>
  */
 public class ElasticsearchDestination implements Destination {
 
+   /**
+    * Our logger.
+    */
    private final static Logger log = LogManager.getLogger(ElasticsearchDestination.class);
 
+   /**
+    * Comma separated list of Elastisearch servers including protocol and port number.
+    */
    private String serverUri;
 
+   /**
+    * Elasticsearch index name.
+    */
    private String index = "perfcake";
 
+   /**
+    * Elasticsearch type name.
+    */
    private String type = "results";
 
+   /**
+    * Comma separated list of tags to be added to results.
+    */
    private String tags = "";
 
+   /**
+    * Elasticsearch user name.
+    */
    private String userName = null;
 
+   /**
+    * Elasticsearch password.
+    */
    private String password = "";
 
+   /**
+    * Elasticsearch client timeout.
+    */
+   private int timeout = 5000;
+
+   /**
+    * To properly search through the data, we need to set the mapping. However, this needs to be done just once for each index and type.
+    */
    private boolean configureMapping = true;
 
+   /**
+    * Time when the test was started.
+    */
    private long startTime;
 
+   /**
+    * Elasticsearch client.
+    */
    private JestClient jest;
 
-   private JsonArray tagsArray = new JsonArray();
+   /**
+    * Requests with reported data.
+    */
+   private ThreadPoolExecutor elasticRequests = (ThreadPoolExecutor) Executors.newFixedThreadPool(1, new ThreadFactoryBuilder().setDaemon(true).build());
 
+   /**
+    * Cached array with tags.
+    */
+   private JsonArray tagsArray = new JsonArray();
 
    @Override
    public void open() {
-      startTime = Long.getLong(System.getProperty(PerfCakeConst.TIMESTAMP_PROPERTY));
+      startTime = Long.getLong(PerfCakeConst.TIMESTAMP_PROPERTY);
 
       final List<String> serverUris = Arrays.asList(serverUri.split(",")).stream().map(StringUtil::trim).collect(Collectors.toList());
       final HttpClientConfig.Builder builder = new HttpClientConfig.Builder(serverUris);
@@ -86,6 +133,10 @@ public class ElasticsearchDestination implements Destination {
       Arrays.asList(tags.split(",")).stream().forEach(tagsArray::add);
 
       builder.multiThreaded(true);
+
+      builder.connTimeout(timeout);
+      builder.readTimeout(timeout);
+      builder.maxTotalConnection(1);
 
       if (userName != null) {
          builder.defaultCredentials(userName, password);
@@ -96,8 +147,9 @@ public class ElasticsearchDestination implements Destination {
       jest = factory.getObject();
 
       if (configureMapping) {
-         final PutMapping mapping = (new PutMapping.Builder(index, type,
-               "{ \"document\" : { "
+         final String mappings =
+               "{ \"mappings\": {"
+                     + " \"" + type + "\": {"
                      + "\"properties\" : { "
                      + "\"" + PeriodType.TIME.toString().toLowerCase() + "\" : {"
                      + "\"type\" : \"date\", "
@@ -109,9 +161,19 @@ public class ElasticsearchDestination implements Destination {
                      + "} "
                      + "} "
                      + "} "
-                     + "}")).build();
+                     + "} "
+                     + "}";
          try {
-            jest.execute(mapping);
+            final JestResult result = jest.execute(new CreateIndex.Builder(index).settings(mappings).build());
+            if (result.isSucceeded()) {
+               log.info("Correctly configured mapping.");
+            } else {
+               if (result.getErrorMessage().contains("index_already_exists")) {
+                  log.warn("Index already exists, cannot re-configure mapping.");
+               } else {
+                  throw new IOException(result.getErrorMessage());
+               }
+            }
          } catch (IOException e) {
             log.warn("Unable to configure mapping: ", e);
          }
@@ -120,6 +182,16 @@ public class ElasticsearchDestination implements Destination {
 
    @Override
    public void close() {
+      elasticRequests.shutdown();
+      try {
+         if (elasticRequests.getQueue().size() > 0 || elasticRequests.getActiveCount() > 0) {
+            log.info("Waiting to send all results to Elasticsearch...");
+         }
+         elasticRequests.awaitTermination(30, TimeUnit.SECONDS);
+      } catch (InterruptedException e) {
+         log.error("Could not write all results to Elasticsearch: ", e);
+      }
+
       jest.shutdownClient();
    }
 
@@ -145,72 +217,167 @@ public class ElasticsearchDestination implements Destination {
 
       final Index indexInstance = new Index.Builder(jsonObject.toString()).index(index).type(type).build();
 
-      jest.executeAsync(indexInstance, new JestResultHandler<DocumentResult>() {
-         @Override
-         public void completed(final DocumentResult result) {
-            // ignore
-         }
-
-         @Override
-         public void failed(final Exception ex) {
-            log.error("Unable to write results to Elasticsearch: ", ex);
+      // built-in async client constantly timeouts, it seems to ignore timeout setting
+      elasticRequests.submit(() -> {
+         try {
+            jest.execute(indexInstance);
+         } catch (IOException ioe) {
+            log.error("Unable to write results to Elasticsearch: ", ioe);
          }
       });
    }
 
+   /**
+    * Gets the comma separated list of Elastisearch servers including protocol and port number.
+    *
+    * @return The comma separated list of Elastisearch servers including protocol and port number.
+    */
    public String getServerUri() {
       return serverUri;
    }
 
+   /**
+    * Sets the comma separated list of Elastisearch servers including protocol and port number.
+    *
+    * @param serverUri
+    *       The comma separated list of Elastisearch servers including protocol and port number.
+    */
    public void setServerUri(final String serverUri) {
       this.serverUri = serverUri;
    }
 
+   /**
+    * Gets the Elasticsearch index name.
+    *
+    * @return The Elasticsearch index name.
+    */
    public String getIndex() {
       return index;
    }
 
+   /**
+    * Sets the Elasticsearch index name.
+    *
+    * @param index
+    *       The Elasticsearch index name.
+    */
    public void setIndex(final String index) {
       this.index = index;
    }
 
+   /**
+    * Gets the Elasticsearch type name.
+    *
+    * @return the Elasticsearch type name.
+    */
    public String getType() {
       return type;
    }
 
+   /**
+    * Sets the Elasticsearch type name.
+    *
+    * @param type
+    *       the Elasticsearch type name.
+    */
    public void setType(final String type) {
       this.type = type;
    }
 
+   /**
+    * Gets the comma separated list of tags to be added to results.
+    *
+    * @return The comma separated list of tags to be added to results.
+    */
    public String getTags() {
       return tags;
    }
 
+   /**
+    * Sets the comma separated list of tags to be added to results.
+    *
+    * @param tags
+    *       The comma separated list of tags to be added to results.
+    */
    public void setTags(final String tags) {
       this.tags = tags;
    }
 
+   /**
+    * Gets the Elasticsearch user name.
+    *
+    * @return The Elasticsearch user name.
+    */
    public String getUserName() {
       return userName;
    }
 
+   /**
+    * Sets the Elasticsearch user name.
+    *
+    * @param userName
+    *       The Elasticsearch user name.
+    */
    public void setUserName(final String userName) {
       this.userName = userName;
    }
 
+   /**
+    * Gets the Elasticsearch password.
+    *
+    * @return The Elasticsearch password.
+    */
    public String getPassword() {
       return password;
    }
 
+   /**
+    * Sets the Elasticsearch password.
+    *
+    * @param password
+    *       The Elasticsearch password.
+    */
    public void setPassword(final String password) {
       this.password = password;
    }
 
+   /**
+    * Gets whether the mapping of data should be configured prior to writing. To properly search through the data,
+    * we need to set the mapping. However, this needs to be done just once for each index and type.
+    *
+    * @return True if and only if the mapping will be configured prior to writing.
+    */
    public boolean isConfigureMapping() {
       return configureMapping;
    }
 
+   /**
+    * Sets whether the mapping of data should be configured prior to writing. To properly search through the data,
+    * we need to set the mapping. However, this needs to be done just once for each index and type.
+    *
+    * @param configureMapping
+    *       True if the mapping should be configured prior to writing.
+    */
    public void setConfigureMapping(final boolean configureMapping) {
       this.configureMapping = configureMapping;
+   }
+
+   /**
+    * Gets the Elasticsearch client timeout.
+    *
+    * @return The Elasticsearch client timeout.
+    */
+   public int getTimeout() {
+      return timeout;
+   }
+
+   /**
+    * Sets the Elasticsearch client timeout.
+    *
+    * @param timeout
+    *       The Elasticsearch client timeout.
+    */
+   public void setTimeout(final int timeout) {
+      this.timeout = timeout;
    }
 }
