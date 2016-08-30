0e67b390d8152404581bbb2cd5eddaec5c1e50a7
Martin Večeřa <marvenec@gmail.com>
2016-08-30 11:00:42 +0200

    #357 added bar type

--- perfcake/src/main/java/org/perfcake/reporting/destination/c3chart/C3ChartHelper.java
+++ perfcake/src/main/java/org/perfcake/reporting/destination/c3chart/C3ChartHelper.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -97,6 +97,7 @@ public class C3ChartHelper {
          chart.setxAxis(chartDestination.getxAxis());
          chart.setyAxis(chartDestination.getyAxis());
          chart.setHeight(chartDestination.getChartHeight());
+         chart.setType(chartDestination.getType());
 
          chartDataFile = new C3ChartDataFile(chart, chartDestination.getOutputDirAsPath());
          chartDataFile.open();
