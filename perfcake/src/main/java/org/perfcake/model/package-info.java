906d598929b6909d00220dec75cba89a792a8855
Martin Večeřa <mvecera@redhat.com>
2016-09-21 17:54:10 +0200

    fixed version glitches

--- perfcake/src/main/java/org/perfcake/model/package-info.java
+++ perfcake/src/main/java/org/perfcake/model/package-info.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,5 +24,5 @@
 // Generated on: 2016.08.17 at 08:30:59 PM CEST 
 //
 
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:perfcake:scenario:6.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:perfcake:scenario:7.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package org.perfcake.model;
