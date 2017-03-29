963972b92e8f46f465bc194376c1394fa11528e9
Martina Kurova <kurovamartina@gmail.com>
2017-03-29 05:07:33 +0200

    Add basic synchronization of threads and master thread election process.
    Add structure shared among all the threads for storing measured results from all running tests. Add a process for electing the master thread that will be responsible for creating a final report.

---
+++ perfcake/src/main/java/org/perfcake/reporting/destination/CrystalDestination.java
@@ -0,0 +1,198 @@
+/*
+ * -----------------------------------------------------------------------\
+ * PerfCake
+ *  
+ * Copyright (C) 2010 - 2016 the original author or authors.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------/
+ */
+package org.perfcake.reporting.destination;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.perfcake.reporting.Measurement;
+import org.perfcake.reporting.ReportingException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Appends a {@link Measurement} into a crystal report file.
+ *
+ * @author <a href="mailto:kurovamartina@gmail.com">Martina Kůrová</a>
+ */
+public class CrystalDestination extends AbstractDestination {
+
+   /**
+   * Logger.
+   */
+   private static final Logger log = LogManager.getLogger(CrystalDestination.class);
+
+   /**
+   * Caching the state of trace logging level to speed up reporting.
+   */
+   private static final boolean logTrace = log.isTraceEnabled();
+
+   /**
+    * A structure for storing measurements, shared among all the threads.
+    */
+   private static ConcurrentHashMap<String, List<Measurement>> sharedResultsMap = new ConcurrentHashMap();
+
+   /**
+    * A structure for electing the master thread.
+    */
+   private static Map<String, Boolean> finishedThreadList = new HashMap<>();
+
+   /**
+    * A lock for accessing the finishedThreadList.
+    */
+   ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+   /**
+    * An elected master thread name.
+    */
+   private static String masterThreadName = null;
+
+   /**
+    * A master thread flag.
+    */
+   private boolean masterThread = false;
+
+   /**
+    * An unique key describing a specific result set.
+    */
+   private String resultSetKey = null;
+
+   /**
+    * The path of a final report.
+    */
+   private String path = null;
+
+   /**
+    * The title of a result in the final report.
+    */
+   private String perfTestName = null;
+
+   /**
+    * The order of the interpreted result within the final report.
+    */
+   private Integer order = null;
+
+   @Override
+   public void open() {
+      resultSetKey = perfTestName;
+      sharedResultsMap.putIfAbsent(resultSetKey, new ArrayList<>());
+
+      readWriteLock.writeLock().lock();
+      finishedThreadList.put(resultSetKey, false);
+      readWriteLock.writeLock().unlock();
+   }
+
+   @Override
+   public void close() {
+      readWriteLock.writeLock().lock();
+      finishedThreadList.replace(resultSetKey, true);
+      readWriteLock.writeLock().unlock();
+
+      // last thread
+      readWriteLock.readLock().lock();
+      masterThread = !finishedThreadList.containsValue(Boolean.FALSE);
+      readWriteLock.readLock().unlock();
+      if (masterThread) {
+         masterThreadName = resultSetKey;
+         generateUnifiedReport();
+      }
+   }
+
+   /**
+    * Generates the final unified report by master thread.
+    */
+   private void generateUnifiedReport() {
+   }
+
+   @Override
+   public void report(final Measurement measurement) throws ReportingException {
+      // store measurement into shared map based on the result set key
+      sharedResultsMap.get(resultSetKey).add(measurement);
+   }
+
+   /**
+    * Gets the path of a final report.
+    *
+    * @return The final report file path.
+    */
+   public String getPath() {
+      return path;
+   }
+
+   /**
+    * Sets the the path of a final report.
+    *
+    * @param path
+    *       The path string.
+    *
+    * @return Instance of this to support fluent API.
+    */
+   public CrystalDestination setPath(String path) {
+      this.path = path;
+      return this;
+   }
+
+   /**
+    * Gets the name of the chart.
+    *
+    * @return The name of the chart.
+    */
+   public String getName() {
+      return perfTestName;
+   }
+
+   /**
+    * Sets the name of the chart.
+    *
+    * @param name
+    *       The name of the chart.
+    * @return Instance of this to support fluent API.
+    */
+   public CrystalDestination setName(final String name) {
+      this.perfTestName = name;
+      return this;
+   }
+
+   /**
+    * Gets the required order of test result within the final report.
+    *
+    * @return The order of test result.
+    */
+   public Integer getOrder() {
+      return order;
+   }
+
+   /**
+    * Sets the order of test result within the final report.
+    *
+    * @param order
+    *   The order of test result.
+    * @return Instance of this to support fluent API.
+    */
+   public CrystalDestination setOrder(Integer order) {
+      this.order = order;
+      return this;
+   }
+}
