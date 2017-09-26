1a0f1c8c6d595b931e775d654b0a6cd62dfa5d3a
Pavel Macík <pavel.macik@gmail.com>
2017-09-26 09:49:58 +0200

    #388: Javadoc for core and agent included into releases.

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 # Abort on Error
 #set -e
 
-if [ -z $TRAVIS_TAG]; then
+if [ -z $TRAVIS_TAG ]; then
 	echo "TRAVIS_TAG variable not set! Skipping..."
 	exit 0;
 fi
@@ -17,17 +17,20 @@ export MAVEN_OPTS="-Xmx326m";
 # My build is using maven, but you could build anything with this, E.g.
 
 mvn install -DskipTests
-mvn -pl perfcake package assembly:single -DskipTests
-mvn -pl perfcake-agent package assembly:single -DskipTests
+mvn -pl perfcake package javadoc:javadoc assembly:single -DskipTests
+mvn -pl perfcake-agent package javadoc:javadoc assembly:single -DskipTests
 
 export PCV=${TRAVIS_TAG#*v}
 
 cp -vf ./perfcake/target/perfcake-$PCV-bin.zip perfcake-$TRAVIS_TAG-bin.zip
 cp -vf ./perfcake/target/perfcake-$PCV-bin.tar.gz perfcake-$TRAVIS_TAG-bin.tar.gz
 cp -vf ./perfcake/target/perfcake-$PCV-bin.tar.bz2 perfcake-$TRAVIS_TAG-bin.tar.bz2
+cp -vf ./perfcake/target/perfcake-$PCV-javadoc.jar perfcake-$TRAVIS_TAG-javadoc.jar
+
 cp -vf ./perfcake-agent/target/perfcake-agent-$PCV-bin-agent.zip perfcake-agent-$TRAVIS_TAG-bin.zip
 cp -vf ./perfcake-agent/target/perfcake-agent-$PCV-bin-agent.tar.gz perfcake-agent-$TRAVIS_TAG-bin.tar.gz
 cp -vf ./perfcake-agent/target/perfcake-agent-$PCV-bin-agent.tar.bz2 perfcake-agent-$TRAVIS_TAG-bin.tar.bz2
+cp -vf ./perfcake-agent/target/perfcake-agent-$PCV-javadoc.jar perfcake-agent-$TRAVIS_TAG-javadoc.jar
 
 #maven_exit=$?
 #if [[ $maven_exit -ne 0 ]];
